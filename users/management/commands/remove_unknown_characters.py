644fe874751068de7d2d93aadced67271bb1ce3c
Smn-20 <rusekezasp@gmail.com>
2025-05-09 15:35:26 +0200

    added new lines

--- users/management/commands/remove_unknown_characters.py
+++ users/management/commands/remove_unknown_characters.py
@@ -1,24 +1,19 @@
-import re
 from django.core.management.base import BaseCommand
 from users.models import Attribute
 
 class Command(BaseCommand):
-    help = "Replace ' • ' with HTML or plain text line breaks depending on content type."
+    help = "Replace ' • ' with <br>•&nbsp; or \\r\\n•\\t depending on whether </p> is in the content."
 
     def handle(self, *args, **kwargs):
         updated = 0
-        # Matches: ' • ' between two letters
-        pattern = re.compile(r'(?<=[a-zA-Z]) • (?=[a-zA-Z])')
 
         for attr in Attribute.objects.all():
             original_content = attr.content
 
             if '</p>' in original_content:
-                # Use HTML line break
-                new_content = pattern.sub('<br>•&nbsp;', original_content)
+                new_content = original_content.replace(' • ', '<br>•&nbsp;')
             else:
-                # Use plain text line break
-                new_content = pattern.sub('\r\n•\t', original_content)
+                new_content = original_content.replace(' • ', '\r\n•\t')
 
             if new_content != original_content:
                 attr.content = new_content
